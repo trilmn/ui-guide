412a54cdf2482a1edd82fe5009608942915c884d
Thien Do <dvkndn@gmail.com>
2018-08-09 15:50:38 +0700

    Update baseUrl

--- core/src/main/scala/anduin/guide/Main.scala
+++ core/src/main/scala/anduin/guide/Main.scala
@@ -4,9 +4,7 @@ import japgolly.scalajs.react.extra.router.{BaseUrl, Router => RawRouter}
 import org.scalajs.dom
 
 object Main {
-  private val isLocal = dom.window.location.hostname == "localhost"
-  private val baseUrlPath = if (isLocal) "" else "ui-guide/"
-  private val baseUrl = BaseUrl.fromWindowOrigin / baseUrlPath
+  private val baseUrl = BaseUrl.fromWindowOrigin
 
   def main(args: Array[String]): Unit = {
     val container = dom.document.getElementById("root")

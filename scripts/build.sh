6b988280ef183151d9f8c71767081b4e6f5077cf
Thien Do <dvkndn@gmail.com>
2018-08-23 23:56:59 +0700

    Add CNAME

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,16 @@
 
 . ./scripts/_prepare.sh
 
+
 # Resources
 echo "[info] Copying resources ..."
 cp ${core_res}/404.html ${docs}/
 cp ${core_res}/index-opt.html ${docs}/index.html
 cp ${core_res}/icon.png ${docs}/
+cp ${core_res}/CNAME ${docs}/
+
+# CSS
+echo "[info] Compiling CSS ..."
 postcss ${core_css_main} --config ${postcss_config} --output ${docs}/app.css
 
 # JS

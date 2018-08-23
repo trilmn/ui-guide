41e37db667408ef3d13d91cee0984df626bdfed3
Thien Do <dvkndn@gmail.com>
2018-08-24 00:14:07 +0700

    Fix build

--- scripts/_prepare.sh
+++ scripts/_prepare.sh
@@ -4,14 +4,9 @@ core_src=./core/src/main
 
 core_res=${core_src}/resources
 core_css=${core_res}/stylesheets
-core_css_tachyons=${core_css}/tachyons
-core_css_base=${core_css}/_base.scss
 core_css_main=${core_css}/main.scss
-core_css_fastopt=${core_css}/core-fastopt.css
 
 core_js=${core_src}/scala/anduin
-core_js_style=${core_js}/style
-core_js_component=${core_js}/component
 
 core_target=./core/target/scala-2.12
 
--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,22 +2,16 @@
 
 . ./scripts/_prepare.sh
 
-echo "[info] Clearing linked JS: anduin.style ..."
-rm -rf ${core_js_style}
-
-echo "[info] Clearing linked JS: anduin.component ..."
-rm -rf ${core_js_component}
+echo "[info] Clearing linked JS ..."
+rm -rf ${core_js}/style
+rm -rf ${core_js}/component
 
 echo "[info] Clearing linked CSS ..."
 rm -rf ${core_css}/tachyons
 rm -f ${core_css}/_base.scss
 
-echo "[info] Clearing generated: development ..."
-rm -f ${core_css_fastopt}
-
 echo "[info] Clearing generated: production ..."
 rm -rf ${docs}
-mkdir ${docs}
 
 echo "[info] Clearing sbt (target) ..."
 sbt clean >/dev/null 2>&1

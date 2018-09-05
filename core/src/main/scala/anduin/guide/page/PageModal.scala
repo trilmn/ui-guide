2d757a5b66184c00e829f2d4e9185099135c0b35
Thien Do <dvkndn@gmail.com>
2018-09-05 12:27:30 +0700

    wip

--- core/src/main/scala/anduin/guide/page/PageModal.scala
+++ core/src/main/scala/anduin/guide/page/PageModal.scala
@@ -1,7 +1,8 @@
 package anduin.guide.page
 
+import anduin.component.button.Button
+import anduin.component.portal.Modal
 import japgolly.scalajs.react.vdom.html_<^._
-
 import anduin.guide.Router
 import anduin.mcro.Source
 import anduin.style.Style
@@ -13,7 +14,20 @@ object PageModal {
       <.header(
         Style.margin.bottom32,
         Header(title = "Modal")()
-      )
+      ),
+      Markdown(
+        """
+          |# Basic
+        """.stripMargin
+      )(),
+      ExampleRich(Source.annotate({
+        val modal = Modal(
+          title = "Hello",
+          renderContent = _ => "Content",
+          renderTarget = toggle => Button(onClick = toggle)("Target")
+        )()
+        <.div(modal)
+      }))()
     )
   }
 }

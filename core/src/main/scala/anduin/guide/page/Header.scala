02116b688e26238fea27a94b6af2754317dc3ef2
Thien Do <dvkndn@gmail.com>
2018-09-27 11:57:52 +0700

    Core: Header supports package name

--- core/src/main/scala/anduin/guide/page/Header.scala
+++ core/src/main/scala/anduin/guide/page/Header.scala
@@ -3,34 +3,39 @@ package anduin.guide.page
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
 
+import anduin.component.text.Tag
+import anduin.guide.component.CodeBlock
 import anduin.style.Style
 
 final case class Header(
   title: String,
-  description: String = ""
+  description: String = "",
+  cls: Option[Class[_]] = None
 ) {
-  def apply(): VdomElement = {
-    Header.component(this)
-  }
+  def apply(): VdomElement = Header.component(this)
 }
 
 object Header {
 
-  private val ComponentName = this.getClass.getSimpleName
+  private type Props = Header
 
-  private case class Backend(scope: BackendScope[Header, _]) {
-    def render(props: Header): VdomElement = {
-      <.div(
-        Style.border.bottom.borderWidth.px2.borderColor.gray2,
-        <.h1(Style.margin.bottom32, props.title),
-        TagMod.when(props.description.nonEmpty) { <.p(Style.margin.bottom32, props.description) }
-      )
-    }
+  private def render(props: Props): VdomElement = {
+    <.div(
+      Style.border.bottom.borderWidth.px2.borderColor.gray2,
+      <.h1(Style.margin.bottom32, props.title),
+      props.cls.whenDefined(cls => {
+        val name = cls.getName.replace("$", "")
+        <.div(Style.margin.bottom32, CodeBlock(name)())
+      }),
+      TagMod.when(props.description.nonEmpty) {
+        <.p(Style.margin.bottom32, props.description)
+      }
+    )
   }
 
   private val component = ScalaComponent
-    .builder[Header](ComponentName)
+    .builder[Props](this.getClass.getSimpleName)
     .stateless
-    .renderBackend[Backend]
+    .render_P(render)
     .build
 }

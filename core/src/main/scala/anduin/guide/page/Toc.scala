caed3b487ed9facc880dea3593990536bb953f42
Thien Do <dvkndn@gmail.com>
2018-09-27 12:06:16 +0700

    Core: Improve TOC implementation

--- core/src/main/scala/anduin/guide/page/Toc.scala
+++ core/src/main/scala/anduin/guide/page/Toc.scala
@@ -9,42 +9,38 @@ import anduin.style.Style
 final case class Toc(
   content: List[(Int, String)]
 ) {
-  def apply(): VdomElement = {
-    Toc.component(this)
-  }
+  def apply(): VdomElement = Toc.component(this)
 }
 
 object Toc {
 
-  private val ComponentName = this.getClass.getSimpleName
+  private type Props = Toc
 
-  private case class Backend(scope: BackendScope[Toc, _]) {
-    def render(props: Toc): VdomElement = {
-      <.div(
-        Style.position.fixed.coordinate.top0,
-        Style.height.pc100.overflow.auto,
-        Style.fontSize.px12.lineHeight.ratio1p5.backgroundColor.white,
-        ^.width := "224px",
-        ^.padding := "64px 16px 64px 0",
-        ^.right := "12px", // Avoid right scrollbar
-        props.content.toVdomArray {
-          case (level, rawText) =>
-            val text = rawText.replace("`", "")
-            <.a(
-              Style.color.inherit.display.block.padding.ver4,
-              ^.key := text,
-              ^.marginLeft := s"${level * 16}px",
-              ^.href := s"#${Heading.getId(text)}",
-              text
-            )
-        }
-      )
-    }
+  private def render(props: Props): VdomElement = {
+    <.div(
+      Style.position.fixed.coordinate.top0,
+      Style.height.pc100.overflow.auto,
+      Style.fontSize.px12.lineHeight.ratio1p5.backgroundColor.white,
+      ^.width := "224px",
+      ^.padding := "64px 16px 64px 0",
+      ^.right := "12px", // Avoid right scrollbar
+      props.content.toVdomArray {
+        case (level, rawText) =>
+          val text = rawText.replace("`", "")
+          <.a(
+            Style.color.inherit.display.block.padding.ver4,
+            ^.key := text,
+            ^.marginLeft := s"${level * 16}px",
+            ^.href := s"#${Heading.getId(text)}",
+            text
+          )
+      }
+    )
   }
 
   private val component = ScalaComponent
-    .builder[Toc](ComponentName)
+    .builder[Props](this.getClass.getSimpleName)
     .stateless
-    .renderBackend[Backend]
+    .render_P(render)
     .build
 }

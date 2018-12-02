dd9f2fe811a1fdfcd64c223c87c1d368ef6ac676
Thien Do <dvkndn@gmail.com>
2018-12-02 16:46:42 +0700

    fix: The mystery bug of QuoteBlock

--- core/src/main/scala/anduin/guide/components/QuoteBlock.scala
+++ core/src/main/scala/anduin/guide/components/QuoteBlock.scala
@@ -14,7 +14,7 @@ object QuoteBlock {
 
   private type Props = QuoteBlock
 
-  private def getColors(key: String) = key match {
+  private def getColors(key: String): TagMod = key match {
     case "primary" => Style.backgroundColor.primary1.borderColor.primary4
     case "success" => Style.backgroundColor.success1.borderColor.success4
     case "warning" => Style.backgroundColor.warning1.borderColor.warning4
@@ -24,17 +24,19 @@ object QuoteBlock {
 
   private def render(props: QuoteBlock): VdomElement = {
     val parts = props.content.split("::")
-    val (styles: TagMod, content) = if (parts.isDefinedAt(1)) {
+    val (styles: TagMod, content: String) = if (parts.isDefinedAt(1)) {
       // well
       val key = parts(1)
       (getColors(key), props.content.replace(s"::$key::", ""))
     } else {
       // block quote
-      (Style.fontStyle.italic.borderColor.gray4, props.content)
+      // IMPORTANT: I don't know why but without TagMod here we have run-time
+      // error in Markdown
+      (TagMod(Style.fontStyle.italic.borderColor.gray4), props.content)
     }
     <.blockquote(
       styles,
-      Style.padding.hor24.padding.ver16.border.left.borderWidth.px4,
+      Style.padding.hor24.border.left.borderWidth.px4,
       ^.dangerouslySetInnerHtml := content
     )
   }

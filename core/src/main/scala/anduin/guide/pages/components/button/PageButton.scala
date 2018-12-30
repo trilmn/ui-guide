ecf1dfd90f13dac9466257976feeffd5bc6edc76
Thien Do <dvkndn@gmail.com>
2018-12-31 01:12:39 +0700

    update button type doc

--- core/src/main/scala/anduin/guide/pages/components/button/PageButton.scala
+++ core/src/main/scala/anduin/guide/pages/components/button/PageButton.scala
@@ -1,40 +1,175 @@
 package anduin.guide.pages.components.button
 
-import anduin.guide.components._
 import anduin.component.button.Button
+import anduin.component.input.textbox.TextBox
 import anduin.guide.app.main.Pages
+import anduin.guide.components._
 import anduin.mcro.Source
 import anduin.style.Style
+import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
-import japgolly.scalajs.react.{Callback, ReactEventFromInput}
 
 object PageButton {
   def render(ctl: Pages.Ctl): VdomElement = {
     <.div(
       Header("Button", Some(Button))(),
       Toc(headings = Source.getTocHeadings)(),
+      Markdown(
+        """
+          |Button lets users take action:
+          |""".stripMargin
+      )(),
       ExampleRich(Source.annotate({
-        <.div(
-          Style.flexbox.flex,
-          <.div(
-            Button(
-              onClick = Callback.alert("You cancelled")
-            )("Cancel")
-          ),
-          <.div(
-            Style.margin.left8,
-            Button(
-              style = Button.Style.Full(color = Button.Color.Blue),
-              onClick = Callback.alert("You submitted")
-            )("Submit")
-          ),
-        )
+        Button(
+          onClick = Callback.alert("Hello")
+        )("Say Hi")
+      }))(),
+      Markdown(
+        """
+          |# Type
+          |
+          |```scala
+          |tpe: Button.Tpe = Button.Tpe.TpeButton
+          |```
+          |
+          |The `tpe` prop defines the behaviour of a Button: whether it
+          |should work like a [link](#type-link), a [submit button](#type-submit)
+          |or a [normal button](#type-button). The `tpe` prop does not affect
+          |the appearance of a Button.
+          |
+          |""".stripMargin
+      )(),
+      Markdown(
+        """
+          |## Type Button
+          |
+          |```scala
+          |Button.Tpe.TpeButton(isAutoFocus: Boolean = false)
+          |// same as <.button(^.tpe := "button")
+          |```
+          |
+          |The default value of `tpe` is `Tpe.TpeButton`, which has no
+          |built-in behaviour. They usually have the actual action defined via
+          |the `onClick` or `onDoubleClick` prop:
+          |
+          |""".stripMargin
+      )(),
+      ExampleRich(Source.annotate({
+        Button(
+          tpe = Button.Tpe.TpeButton(), // usually omitted
+          onClick = Callback.alert("You clicked!")
+        )("Click me")
+      }))(),
+      Markdown(
+        """
+          |## Type Submit
+          |
+          |```scala
+          |Button.Tpe.Submit(isAutoFocus: Boolean = false)
+          |// same as <.button(^.tpe := "submit")
+          |```
+          |
+          |The `Submit` option will trigger the `onSubmit` of the associated
+          |[form][mdn-form] when users click on the Button. This is a native,
+          |built-in behaviour so no `onClick` needs to be defined:
+          |
+          |[mdn-form]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/form
+          |
+          |""".stripMargin
+      )(),
+      ExampleRich(Source.annotate({
+        SimpleState.Str(
+          initialValue = "",
+          render = (value, setValue) => {
+            val text = TextBox(value, setValue, placeholder = "Name...")()
+            val button = Button(tpe = Button.Tpe.Submit())("Submit")
+            <.form(
+              Style.flexbox.flex,
+              ^.onSubmit ==> (_.preventDefaultCB >> Callback.alert("a")),
+              <.div(Style.width.px128, text),
+              <.div(Style.margin.left8, button)
+            )
+          }
+        )()
       }))(),
       Markdown(
         s"""
-           |# Usage
+           |Whenever possible, prefer form's `onSubmit` over Button's
+           |`onClick` so user can submit via several ways. For example, try
+           |pressing "Enter" while you are still in the Text Box above!
+           |
+           |""".stripMargin
+      )(),
+      Markdown(
+        s"""
+           |
+           |## Type Link
+           |
+           |```scala
+           |Button.Tpe.Link(
+           |  href: String,
+           |  target: Button.Target = Button.Target.Self
+           |)
+           |// same as <.a(^.href := ...)
+           |```
+           |
+           |The `Link` option renders an actual [`anchor` element][mdn-a] and
+           |thus should be used when the Button's target is a page, file,
+           |location on a page, email address, or other URL:
+           |
+           |[mdn-a]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a
+           |
+           |""".stripMargin
+      )(),
+      ExampleRich(Source.annotate({
+        Button(
+          tpe = Button.Tpe.Link("https://anduin.design")
+        )("Go to anduin.design")
+      }))(),
+      Markdown(
+        """
+          |The `Link` option has a `target` parameter, which is the
+          |equivalent of the [`target` attribute](mdn-target) of the `anchor`
+          |element. It can be `Self` (default), `Blank` or `Parent`:
+          |
+          |[mdn-target]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#attr-target
+          |
+          |""".stripMargin
+      )(),
+      ExampleRich(Source.annotate({
+        Button(
+          tpe = Button.Tpe.Link(
+            href = "https://anduin.design",
+            target = Button.Target.Blank
+          )
+        )("Open anduin.design")
+      }))(),
+      Markdown(
+        s"""
+           |
+           |# Style
+           |
+           |## Style Full
+           |
+           |""".stripMargin
+      )(),
+      ExampleRich(Source.annotate({
+        Button(
+          )()
+      }))(),
+      Markdown(
+        s"""
+           |
+           |## Style Ghost
+           |
+           |## Style Minimal
+           |
+           |## Style Link
+           |
+           |# Disabled
            |
            |The Button component should be used where we would use the native HTML's [button](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button) tag. That is, for in-page actions such as:
+           |
            |- Submit or reset a form
            |- Show a modal, popover, or menu
            |- Add or remove element from a list
@@ -47,23 +182,6 @@ object PageButton {
            |
            |[1]: ${ctl.urlFor(Pages.ButtonLink()).value}
            |
-           |# Appearance
-           |
-           |Button supports all customization via ButtonStyle. See the [ButtonStyle guide][2] for detail.
-           |
-           |[2]: ${ctl.urlFor(Pages.ButtonStyle()).value}
-           |
-           |# Type
-           |
-           |Like the native `button` tag, the Button component has a `tpe` prop with 3 options:
-           |
-           |- `TpeButton` (equivalent to `type=button`)
-           |- `TpeReset` (`type=reset`)
-           |- `TpeSubmit` (`type=submit`).
-           |
-           |**The default value is `TpeButton`,** which does nothing out of the box. You often need to provide an `onClick` handler to make it do something.
-           |
-           |Meanwhile, `TpeReset` and `TpeSubmit` have useful native function when used inside a `form` (e.g. `TpeSubmit` triggers form's `onSubmit`). You often don't need to provide `onClick` for these 2 types.
           """.stripMargin
       )(),
       ExampleRich(Source.annotate({
@@ -85,7 +203,6 @@ object PageButton {
       }))(),
       Markdown(
         s"""
-           |# `isDisabled`
            |
            |Since these are actual `button` tags, we can prevent users from interacting with them via the `isDisabled` prop.
            |
